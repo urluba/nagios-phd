25c751b91c5759d1894c79d2959c49d965115b44
urluba <lucas.barriere@gmail.com>
2016-12-16 16:29:39 +0000

    FIX	Set default loglvl to ERROR. Have to read Begins doc now...

--- module/nagios-phd.py
+++ module/nagios-phd.py
@@ -7,43 +7,39 @@ import begin
 import json
 import logging
 import boto3
-from botocore.client import ClientError
 
 NAGIOS_STATUS_OK = 0
 NAGIOS_STATUS_WARNING = 1
 NAGIOS_STATUS_CRITICAL = 2
 NAGIOS_STATUS_UNKNOWN = 3
 
-# Time to debug...
-logging.basicConfig(level=logging.WARNING)
-logger = logging.getLogger(__name__)
-
-
 def _init_aws_session(boto_profile='int'):
     # Let's go
     try:
         session = boto3.Session(profile_name=boto_profile)
     except ProfileNotFound as e:
-        logger.error(e)
+        logging.error(e)
         raise SystemExit, 1
 
     return session
 
 def _this_is_the_end(error_code=NAGIOS_STATUS_UNKNOWN, output=u"I know nothing"):
-  logger.debug("+ All done, I quit! (with %s)" % error_code)
+  logging.debug("+ All done, I quit! (with %s)" % error_code)
   print output
   raise SystemExit(error_code)
 
 @begin.start
 @begin.logging
-def run(warning=1, critical=0, username="int", Hostname='eu-west-1', verbose=False ):
-  logger.debug("---------------------------------------------------------")
-  logger.debug("+ C'est parti")
+def run(warning=1, critical=0, username="int", Hostname='eu-west-1', loglvl='ERROR'):
+  "Query AWS Personal Health Dashboard for any open events"
+
+  logging.debug("---------------------------------------------------------")
+  logging.debug("+ C'est parti")
 
   module_output = "Blackhole..."
   module_return_code = NAGIOS_STATUS_UNKNOWN
 
-  logger.debug( "+ Using profile %s" % username)
+  logging.debug( "+ Using profile %s" % username)
   session = _init_aws_session(username)
   client = session.client('health', 'us-east-1') # Only entrypoint for now
 
@@ -51,7 +47,7 @@ def run(warning=1, critical=0, username="int", Hostname='eu-west-1', verbose=Fal
     'regions': [Hostname],
     'eventStatusCodes': ['open'],
   }
-  logger.debug('+ Filters: %s' % filters)
+  logging.debug('+ Filters: %s' % filters)
 
   try:
     '''response = client.describe_event_aggregates(
@@ -64,24 +60,24 @@ def run(warning=1, critical=0, username="int", Hostname='eu-west-1', verbose=Fal
       filter = filters,
     )
   except "AccessDeniedException":
-    logger.error('Profile % is not authorized' % username)
+    logging.error('Profile % is not authorized' % username)
     _this_is_the_end(NAGIOS_STATUS_UNKNOWN, "Unable to query AWS Health")
 
   number_events = len(response['events'])
-  logger.debug('+ %s event(s) matched filters' % number_events)
+  logging.debug('+ %s event(s) matched filters' % number_events)
 
   if number_events == 0:
-    logger.debug('+ Pas un probleme')
+    logging.debug('+ Pas un probleme')
     _this_is_the_end(NAGIOS_STATUS_OK, "So far so good |0")
 
   if warning and number_events >= warning:
     module_return_code = NAGIOS_STATUS_WARNING
-    logger.debug('+ Warning will be returned as above specified threshold'
+    logging.debug('+ Warning will be returned as above specified threshold'
      ' of %s' % warning)
 
   if critical and number_events >= critical:
     module_return_code = NAGIOS_STATUS_CRITICAL
-    logger.debug('+ Critical will be returned as above specified threshold'
+    logging.debug('+ Critical will be returned as above specified threshold'
       ' of %s' % critical)
 
   module_output = u"%s incident(s) en cours |%s\n" % (number_events, number_events)
